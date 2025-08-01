8744e2b24006ef715270d106f1584800a8ee716b
abdullahjaved06 <javedabdullah963@gmail.com>
2025-08-01 12:28:35 +0500

    added logs for temp and hum

--- components/scd41/scd41.c
+++ components/scd41/scd41.c
@@ -4,13 +4,17 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <stdio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(CO2);
+
 
 static const struct device *dev = DEVICE_DT_GET_ONE(sensirion_scd41);
 
 int scd41_device_check(void)
 {
     if (!device_is_ready(dev)) {
-        printf("SCD41 device not ready\n");
+        LOG_ERR("SCD41 device not ready\n");
         return -ENODEV;
     }
     return 0;
@@ -23,18 +27,18 @@ float scd41_read_co2(void)
 
     err = sensor_sample_fetch(dev);
     if (err) {
-        printf("SCD41 fetch error: %d\n", err);
+        LOG_ERR("SCD41 fetch error: %d\n", err);
         return -1.0f;
     }
 
     err = sensor_channel_get(dev, SENSOR_CHAN_CO2, &co2);
     if (err) {
-        printf("SCD41 CO₂ read error: %d\n", err);
+        LOG_ERR("SCD41 CO₂ read error: %d\n", err);
         return -1.0f;
     }
 
     float co2_val = sensor_value_to_double(&co2);
-    printf("CO₂: %.2f ppm\n", co2_val);
+    LOG_INF("CO₂: %.2f ppm\n", co2_val);
 
     return co2_val;
 }
--- components/sht40/sht40.c
+++ components/sht40/sht40.c
@@ -5,12 +5,17 @@
 #include <zephyr/drivers/sensor.h>
 #include <stdio.h>
 
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(TEMP_HUM);
+
 static const struct device *dev = DEVICE_DT_GET_ONE(sensirion_sht4x);
 
 int sht4x_device_check(void)
 {
     if (!device_is_ready(dev)) {
-        printf("SHT4x device not ready\n");
+        LOG_ERR("SHT4x device not ready\n");
         return -ENODEV;
     }
     return 0;
@@ -21,18 +26,26 @@ float sht4x_read_temperature(void)
     struct sensor_value temp;
     if (sensor_sample_fetch(dev) != 0 ||
         sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp) != 0) {
+        LOG_ERR("Failed to read temperature");
         return -1.0f;
     }
-    return sensor_value_to_double(&temp);
+    float t = sensor_value_to_double(&temp);
+    LOG_INF("Temperature: %.2f C", t);
+    return t;
 }
+
 
 float sht4x_read_humidity(void)
 {
     struct sensor_value hum;
     if (sensor_sample_fetch(dev) != 0 ||
         sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum) != 0) {
+        LOG_ERR("Failed to read humidity");
         return -1.0f;
     }
-    return sensor_value_to_double(&hum);
+    float h = sensor_value_to_double(&hum);
+    LOG_INF("Humidity: %.2f %%", h);
+    return h;
 }
+
 
